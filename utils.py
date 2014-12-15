da79383704e2061f2e7cf87d755323dc71b4a76a
zhongbin pan <zhongbin.pan@hp.com>
2014-12-15 16:48:11 +0800

    Beta version is ready
    1. Add Config class
    2. Add MaintenanceEventBroker
    3. Add MaintenanceScheduler class
    4. Add functions to Cache

--- utils.py
+++ utils.py
@@ -11,13 +11,17 @@ from datetime import datetime
 from contextlib import contextmanager
 
 import logging
+import ConfigParser
 from tempfile import mkstemp
 
+LOGGING_FORMAT = '%(asctime)s %(levelname)s user_id:%(user_id)s %(message)s'
+SYSLOG_LOGGING_FORMAT = '%(levelname)s user_id:%(user_id)s %(message)s'
+CONF_PATH = '/etc/polaris/maintenance.cfg'
 
 class DBConnectionError(sqlite3.DatabaseError):
     def __init__(self, path, msg, timeout=0):
         self.path = path
-        self.msg = msg 
+        self.msg = msg
         self.timeout = timeout
 
     def __str__(self):
@@ -31,23 +35,31 @@ class DBAlreadyExists(sqlite3.DatabaseError):
     def __str__(self):
         return 'DB %s already exists' % self.path
 
+class InvalidIDError(Exception):
+            def __init__(self, id):
+                self.id = id
+
+            def __str__(self):
+                return "Invalid id: %d" % self.id
+
 class AuthError(Exception):
-    def __init__(self, token):
+    def __init__(self, user, token):
+        self.user = user
         self.token = token
 
 class InvalidTokenError(AuthError):
     def __str__(self):
-        return "Invalid Token: %s" % self.token 
+        return "invalid token: %s" % self.token
 
 class ExpiredTokenError(AuthError):
     def __str__(self):
-        return "Expired Token: %s" % self.token
+        return "user: %s --- expired token: %s" % (self.user, self.token)
 
 class Cache(object):
     """
     Cache is a deco class for token cache usage
     """
-    cache = {}
+    cache = dict()
     def __init__(self):
         super(Cache, self).__init__()
 
@@ -56,17 +68,85 @@ class Cache(object):
         def wrapper(inst, user, passwd):
             if user not in Cache.cache:
                 Cache.cache[user] = f(inst, user, passwd)
-                self.dump()
             return Cache.cache.get(user)
         return wrapper
 
-    def ishit(self, user, token):
-        return token == self.cache.get(user)
+    @classmethod
+    def remove_token(cls, token):
+        if token in cls.invert_cache():
+            del cls.cache[token]
+        # After deletion, restore to original cache
+        cls.invert_cache()
 
-    def dump(self):
+    @classmethod
+    def invert_cache(cls):
+        """
+        The items of cache should be unique for each user.
+        So it can be inverted for searching via token value.
+        """
+        cls.cache = {v: k for k, v in cls.cache.items()}
+        return cls.cache
+
+    @classmethod
+    def ishit(cls, user, token):
+        return token == cls.cache.get(user)
+
+    @classmethod
+    def dump(cls):
         #TODO
-        for k, v in Cache.cache.items():
+        for k, v in cls.cache.items():
             print k, ": ", v
+
+class Config(object):
+    """
+    To store the configuration set by DevOps.
+    The configuration is defined by groups.
+    One example below:
+        [devops]
+        user = devops
+        passwd = passwd
+
+        [security]
+        algorithm = md5
+        expires = 86400
+
+        [database]
+        auth = /var/polaris/auth.db
+        event = /var/polaris/maintenance.db
+
+    """
+    def __init__(self, conf_file='/etc/polaris/maintenance.cfg'):
+        self.conf_file = conf_file
+        self.config = ConfigParser.RawConfigParser(allow_no_value=True)
+        self.config.read(conf_file)
+
+    @staticmethod
+    def get_config():
+        return Config(CONF_PATH)
+
+    @property
+    def user(self):
+        return self.config.get('devops', 'user')
+
+    @property
+    def passwd(self):
+        return self.config.get('devops', 'passwd')
+
+    @property
+    def algorithm(self):
+        return self.config.get('security', 'algorithm')
+
+    @property
+    def expires(self):
+        return self.config.getint('security', 'expires')
+
+    @property
+    def auth_db(self):
+        return self.config.get('database', 'auth')
+
+    @property
+    def maintenance_event_db(self):
+        return self.config.get('database', 'event')
 
 def mkdirs(path):
     if not os.path.isdir(path):
@@ -90,20 +170,20 @@ def get_db_connection(path, timeout=30):
             stat = os.stat(path)
             if stat.st_size == 0 and stat.st_ctime >= connect_time:
                 os.ulink(path)
-                raise DBConnectionError(path, 'is Invalid DB file') 
+                raise DBConnectionError(path, 'is Invalid DB file')
 
         conn.row_factory = sqlite3.Row
         conn.text_factory = str
         return conn
     except sqlite3.DatabaseError:
         import traceback
-        raise DBConnectionError(path, 
-                                traceback.format_exc(), 
+        raise DBConnectionError(path,
+                                traceback.format_exc(),
                                 timeout=timeout)
 
 class DBBroker(object):
     """
-    DBBroker is top broker class of sqlite3 connection, it should not 
+    DBBroker is top broker class of sqlite3 connection, it should not
     be used directly. Any new table to create, please inherit it for usage.
     @db_file: the db file to connect, should be defined in devops settings.
     """
@@ -131,7 +211,7 @@ class DBBroker(object):
 
     def commit(self):
         self.conn.commit()
-    
+
     def is_table_existing(self, table):
         query = '''
         SELECT name FROM sqlite_master WHERE type='table' AND name = '%s'
@@ -142,15 +222,15 @@ class DBBroker(object):
         """
         Create the database
         """
-        mkdirs(self.db_dir) 
+        mkdirs(self.db_dir)
         fd, tmp_db_file = mkstemp(suffix='.tmp', dir=self.db_dir)
         os.close(fd)
-        conn = sqlite3.connect(tmp_db_file, 
+        conn = sqlite3.connect(tmp_db_file,
                                check_same_thread=False,
                                timeout=0)
         self._initialize(conn)
         conn.commit()
-        if tmp_db_file: 
+        if tmp_db_file:
             conn.close()
             if not os.path.exists(self.db_file):
                 with open(tmp_db_file, 'r+b') as f:
@@ -196,7 +276,7 @@ class AuthBroker(DBBroker):
 
     def _initialize(self, conn):
         self.create_auth_table(conn)
-    
+
     def create_auth_table(self, conn):
         """
         Create the auth table for authentication
@@ -204,9 +284,9 @@ class AuthBroker(DBBroker):
         """
         sql = '''
         CREATE TABLE auth
-        (id INTEGER primary key AUTOINCREMENT,
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
         user varchar UNIQUE,
-        token text,
+        token TEXT,
         expires INTEGER)
         '''
         conn.cursor().execute(sql)
@@ -224,17 +304,105 @@ class MaintenanceEventBroker(DBBroker):
     def create_maintenance_event_table(self, conn):
         sql = '''
         CREATE TABLE maintenance_event
-        (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        service text, 
-        when Date, 
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        service TEXT,
+        timestamp TEXT,
         duration INTEGER)
         '''
         conn.cursor().execute(sql)
 
+    def add_record(self, service_lst, when, duration):
+        """
+        SQLite does not have a storage class set aside for storing dates
+        and/or times. Instead, the built-in Date And Time Functions of
+        SQLite are capable of storing dates and times as TEXT, REAL, or
+        INTEGER values:
+            * TEXT as ISO8601 strings ("YYYY-MM-DD HH:MM:SS.SSS").
+            * REAL as Julian day numbers, the number of days since noon
+              in Greenwich on November 24, 4714 B.C. according to the proleptic
+              Gregorian calendar.
+            * INTEGER as Unix Time, the number of seconds since 1970-01-01
+              00:00:00 UTC.
+        In Polaris-* services, the date format is ISO8601, so we would like to
+        use TEXT as the date type.
+        """
+
+        sql = '''
+        INSERT INTO maintenance_event VALUES
+        (NULL, '%s', '%s', %d)
+        ''' % (service_lst, when, duration)
+        self.execute_sql(sql)
+        self.commit()
+
+    def get_record(self, id):
+        query = '''
+        SELECT * FROM maintenance_event WHERE id = %d
+        ''' % id
+        return self.execute_sql(query).fetchone()
+
+    def get_max_id(self):
+        query = '''
+        SELECT MAX(id) as id FROM maintenance_event
+        '''
+        r = self.execute_sql(query).fetchone()
+        return r['id'] if r else None
+
+    def update_record(self, id, service_lst, when, duration):
+        update = '''
+        (SELECT * FROM maintenance_event WHERE id = %d)
+        ''' % id
+        sql = '''
+        INSERT OR REPLACE INTO maintenance_event VALUES
+        (%s, '%s', '%s', %d)
+        ''' % (update, service_lst, when, duration)
+        try:
+            self.execute_sql(sql)
+            self.commit()
+        except (sqlite3.DataError, sqlite3.DatabaseError):
+            raise
+
+
+class MaintenanceScheduler(object):
+    def __init__(self):
+        super(MaintenanceScheduler, self).__init__()
+        db_file = Config.get_config().maintenance_event_db
+        self._get_broker = lambda : MaintenanceEventBroker(db_file)
+
+    @staticmethod
+    def get_scheduler():
+        return MaintenanceScheduler()
+
     def create_event(self, services, when, duration):
-        pass #TODO
+        broker = self._get_broker()
+        broker.initialize()
+        try:
+            broker.add_record(services, when, duration)
+        except DBConnectionError:
+            broker.add_record(services, when, duration)
+        finally:
+            return broker.get_max_id()
 
     def get_event(self, id):
+        broker = self._get_broker()
+        try:
+            with broker.broker as broker:
+                r = broker.get_record(id)
+                if r:
+                    return r
+                else:
+                    raise InvalidIDError(id)
+        except DBConnectionError:
+            raise
+
+    def update_event(self, id, service_lst, when, duration):
+        broker = self._get_broker()
+        try:
+            broker.update_record(id, service_lst, when, duration)
+            return id
+        except Exception:
+            raise
+
+    def delete_event(self, id):
         pass #TODO
 
 class SimpleAuth(object):
@@ -247,16 +415,24 @@ class SimpleAuth(object):
     """
     def __init__(self, algorithm='md5', expires=86400):
         super(SimpleAuth, self).__init__()
-        self.db_file = '/Users/lafengnan/codes/Github/maintenance/nf.db' # for test, will get from settings
         self.token_life = expires
-        self._get_broker = lambda : AuthBroker(self.db_file)
+        db_file = Config.get_config().auth_db
+        self._get_broker = lambda : AuthBroker(db_file)
         self.hash = getattr(hashlib, algorithm.lower(), hashlib.md5)
+
+    @staticmethod
+    def get_simple_auth():
+        algorithm = Config.get_config().algorithm or 'md5'
+        expires = Config.get_config().expires or 86400
+        return SimpleAuth(algorithm, expires)
 
     @Cache()
     def get_token(self, user, passwd):
         def _validate_user_and_passwd():
-            pass #TODO
-        
+            u = Config.get_config().user
+            p = Config.get_config().passwd
+            if u != user or p != passwd:
+                raise Exception("Invalid (user: %s, passwd: %s)" % (user, passwd))
         try:
             _validate_user_and_passwd()
         except Exception:
@@ -273,7 +449,7 @@ class SimpleAuth(object):
             (SELECT id FROM auth WHERE user = '%s')
             ''' % user
             sql = '''
-            INSERT or REPLACE INTO auth VALUES(%s, '%s', '%s', %d)
+            INSERT OR REPLACE INTO auth VALUES(%s, '%s', '%s', %d)
             ''' % (update, user, token, expires)
             broker.execute_sql(sql)
             broker.commit()
@@ -290,7 +466,7 @@ class SimpleAuth(object):
     def validate_token(self, token):
         def _get_token_info_from_db():
             q = '''
-            SELECT token, expires FROM auth WHERE token = '%s'
+            SELECT user, token, expires FROM auth WHERE token = '%s'
             ''' % token
             # If token is valid, return(token, expires)
             # If token is invalid, return None
@@ -301,14 +477,11 @@ class SimpleAuth(object):
             with broker.broker as broker:
                 info = _get_token_info_from_db()
                 if not info:
-                    raise InvalidTokenError(token)
+                    raise InvalidTokenError(None, token)
                 elif info['expires'] - long(time()) <= 0:
                     # Remove it from cache if it has been cached
-                    for u in Cache.cache:
-                        if Cache.cache[u] == info['token']:
-                            del Cache.cache[u]
-                            break
-                    raise ExpiredTokenError(token)
+                    Cache.remove_token(token)
+                    raise ExpiredTokenError(info['user'], token)
                 else:
                     return "valid"
         except DBConnectionError:
--- utils_test.py
+++ utils_test.py
@@ -48,16 +48,34 @@ def get_token(auth, u, p, algorithm='md5'):
 
 if __name__ == '__main__':
     #main()
-    auth = utils.SimpleAuth()
-    print get_token(auth, 'devops', 'Passw0rd')
-    print get_token(auth, 'devops2', 'Passw0rd')
-    print get_token(auth, 'devops3', 'Passw0rd')
-    print get_token(auth, 'devops4', 'Passw0rd')
-    print get_token(auth, 'devops5', 'Passw0rd')
-    t = get_token(auth, 'devops', 'Passw0rd')
-    print t
-    try:
-        print auth.validate_token(t)
-        print auth.validate_token('123')
-    except utils.InvalidTokenError as e:
-        print e
+    auth = utils.SimpleAuth.get_simple_auth()
+
+    t1 = get_token(auth, 'devops', 'Passw0rd')
+    #t2= get_token(auth, 'devops2', 'Passw0rd')
+    #t3 = get_token(auth, 'devops3', 'Passw0rd')
+    #t4 = get_token(auth, 'devops4', 'Passw0rd')
+    #t5 = get_token(auth, 'devops5', 'Passw0rd')
+    t2 = get_token(auth, 'devops', 'Passw0rd')
+    for t in (t1, t2):
+        print t
+        try:
+            print auth.validate_token(t)
+        except utils.InvalidTokenError as e:
+            print e
+        except utils.ExpiredTokenError as e:
+            print e
+        try:
+            print auth.validate_token('123')
+        except utils.InvalidTokenError as e:
+            pass
+        except utils.ExpiredTokenError as e:
+            pass
+        finally:
+            print e
+
+    sch = utils.MaintenanceScheduler.get_scheduler()
+    id1 = sch.create_event("storage, metadata", "2014-12-31 12:00:00.000", 120)
+    id2 = sch.create_event("storage, ES", "2014-12-30 12:00:00.000", 120)
+    id3 = sch.create_event("storage, Cassandra", "2014-12-30 12:00:00.000", 120)
+    print id1, id2, id3
+    print sch.get_event(-1)
